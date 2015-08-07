2a97569eeb168438101cc055e6e400c2a4dfe8e8
Maochen G <contact@maochen.org>
2015-08-07 19:53:00 -0400

    Work on Chi Square.

---
+++ CoreNLP-Experiment/src/main/java/org/maochen/nlp/ml/feature/chaisquare/ChiSquare.java
@@ -0,0 +1,81 @@
+package org.maochen.nlp.ml.feature.chaisquare;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+import org.apache.commons.math3.distribution.GammaDistribution;
+import org.maochen.nlp.ml.datastructure.Tuple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Created by Maochen on 8/7/15.
+ */
+public class ChiSquare {
+    private static final Logger LOG = LoggerFactory.getLogger(ChiSquare.class);
+
+
+    // Row - feats, Col - label
+    protected Table<String, String, Integer> dataTable = HashBasedTable.create();
+
+    // Value smaller -> independent
+    protected Table<String, String, Double> chiSquareTable = HashBasedTable.create();
+
+    protected int df;
+
+    protected int total; // N
+
+    public double totalChiSquare;
+
+    public double totalPVal;
+
+    private Function<Table<String, String, Integer>, Integer> getTotal = table1 ->
+            table1.rowMap().values().stream().map(Map::values) // List<Collection<Int>>
+                    .map(lst -> lst.stream().mapToInt(num -> num).sum()) // List<Int>
+                    .mapToInt(num -> num).sum();
+
+    public void loadTrainingData(List<Tuple> trainingData) {
+
+        for (Tuple t : trainingData) {
+            String label = t.label;
+
+            for (String featName : t.featureName) {
+                Integer count = dataTable.get(featName, label);
+                count = count == null ? 1 : count + 1;
+                dataTable.put(featName, label, count);
+            }
+        }
+
+    }
+
+    protected static double getPValue(final double chiSquare, double df) {
+        GammaDistribution gamma = new GammaDistribution(df / 2.0D, 2.0D);
+        double gammaVal = gamma.cumulativeProbability(chiSquare);
+
+        return 1 - gammaVal;
+    }
+
+    public void calculateChiSquare() {
+        df = (dataTable.rowKeySet().size() - 1) * (dataTable.columnKeySet().size() - 1);
+        total = getTotal.apply(dataTable);
+        // R, C, V
+        dataTable.cellSet().forEach(cell -> {
+            String feat = cell.getRowKey();
+            String label = cell.getColumnKey();
+            int count = cell.getValue() == null ? 0 : cell.getValue();
+            int c_feat = dataTable.row(feat).values().stream().mapToInt(x -> x).sum();
+            int c_label = dataTable.column(label).values().stream().mapToInt(x -> x).sum();
+            double e_xi_yi = (c_feat * c_label) / (double) total;
+            chiSquareTable.put(feat, label, Math.pow(count - e_xi_yi, 2) / e_xi_yi);
+        });
+
+
+        totalChiSquare = chiSquareTable.cellSet().parallelStream().mapToDouble(cell -> cell.getValue() == null ? 0D : cell.getValue()).sum();
+        totalPVal = getPValue(totalChiSquare, df);
+    }
+
+}
---
+++ CoreNLP-Experiment/src/test/java/org/maochen/nlp/ml/feature/chaisquare/ChiSquareTest.java
@@ -0,0 +1,38 @@
+package org.maochen.nlp.ml.feature.chaisquare;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Maochen on 8/7/15.
+ */
+public class ChiSquareTest {
+
+    @Test
+    public void testPValue() {
+        double actual = ChiSquare.getPValue(16.2, 2);
+        double expected = 3.03539138078901E-4;
+        assertEquals(expected, actual, Double.MIN_NORMAL);
+    }
+
+    @Test
+    public void testChiSquare() {
+        ChiSquare chiSquare = new ChiSquare();
+        chiSquare.dataTable.put("Male", "Rep", 200);
+        chiSquare.dataTable.put("Male", "Demo", 150);
+        chiSquare.dataTable.put("Male", "Indep", 50);
+
+        chiSquare.dataTable.put("Female", "Rep", 250);
+        chiSquare.dataTable.put("Female", "Demo", 300);
+        chiSquare.dataTable.put("Female", "Indep", 50);
+
+        chiSquare.calculateChiSquare();
+
+
+        assertEquals(1000, chiSquare.total);
+        assertEquals(2, chiSquare.df);
+        assertEquals(16.203703703703702, chiSquare.totalChiSquare, Double.MIN_NORMAL);
+        assertEquals(3.0297754871455584E-4, chiSquare.totalPVal, Double.MIN_NORMAL);
+    }
+}
