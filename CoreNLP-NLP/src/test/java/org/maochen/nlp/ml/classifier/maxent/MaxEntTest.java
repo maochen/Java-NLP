e1b36dc5dd1cf36581c5c7bb48d2efa8a9c808b4
Maochen G <contact@maochen.org>
2015-08-07 14:06:05 -0400

    change maxent test accordingly.

--- CoreNLP-NLP/src/test/java/org/maochen/nlp/ml/classifier/maxent/MaxEntTest.java
+++ CoreNLP-NLP/src/test/java/org/maochen/nlp/ml/classifier/maxent/MaxEntTest.java
@@ -3,17 +3,33 @@ package org.maochen.nlp.ml.classifier.maxent;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
- * TODO: Transform
  * Created by Maochen on 8/7/15.
  */
 public class MaxEntTest {
 
+    // tie=0.1899, lose=0.3686, win=0.4416
+    private Map<String, Double> getMap(String s) {
+        Map<String, Double> result = new HashMap<>();
+        String[] tokens = s.split(",");
+        for (String token : tokens) {
+            String[] kv = token.split("=");
+            String key = kv[0].trim();
+            Double value = Double.parseDouble(kv[1].trim());
+            result.put(key, value);
+        }
+        return result;
+    }
+
     @Test
-    public void test() {
+    public void testStringData() {
         List<String[]> traindata = new ArrayList<>();
         traindata.add(new String[]{"away", "pdiff=0.6875", "ptwins=0.5", "lose"});
         traindata.add(new String[]{"away", "pdiff=1.0625", "ptwins=0.5", "win"});
@@ -117,14 +133,16 @@ public class MaxEntTest {
         traindata.add(new String[]{"away", "pdiff=0.9375", "ptwins=0.4545", "lose"});
 
 
-        MaxEntClassifier maxent = new MaxEntClassifier(false);
+        MaxEntClassifier maxent = new MaxEntClassifier();
         maxent.trainString(traindata);
 
 //        maxent.persist(maxent.pathPrefix + "/maxentModel.txt");
 //        maxent.model = null;
 //        maxent.loadModel(maxent.pathPrefix + "/maxentModel.txt");
 
-        List<String[]> predictData = new ArrayList<String[]>();
+        List<String[]> predictData = new ArrayList<>();
+
+
         predictData.add(new String[]{"home", "pdiff=0.6875", "ptwins=0.5"});
         predictData.add(new String[]{"home", "pdiff=1.0625", "ptwins=0.5"});
         predictData.add(new String[]{"away", "pdiff=0.8125", "ptwins=0.5"});
@@ -136,29 +154,28 @@ public class MaxEntTest {
         predictData.add(new String[]{"home", "pdiff=0.9375", "ptwins=0.3333"});
         predictData.add(new String[]{"home", "pdiff=0.6875", "ptwins=0.5"});
 
-//        [home, pdiff=0.6875, ptwins=0.5]
-//        {tie=0.1899, lose=0.3686, win=0.4416}
-//        [home, pdiff=1.0625, ptwins=0.5]
-//        {tie=0.2462, lose=0.3451, win=0.4087}
-//        [away, pdiff=0.8125, ptwins=0.5]
-//        {tie=0.2251, lose=0.5947, win=0.1802}
-//        [away, pdiff=0.6875, ptwins=0.6]
-//        {tie=0.1882, lose=0.6056, win=0.2062}
-//        [home, pdiff=0.9375, ptwins=0.5]
-//        {tie=0.2263, lose=0.3535, win=0.4202}
-//        [home, pdiff=0.6875, ptwins=0.3333]
-//        {tie=0.2305, lose=0.3859, win=0.3836}
-//        [away, pdiff=1.0625, ptwins=0.6666]
-//        {tie=0.2294, lose=0.5656, win=0.205}
-//        [home, pdiff=0.8125, ptwins=0.6666]
-//        {tie=0.1688, lose=0.3409, win=0.4903}
-//        [home, pdiff=0.9375, ptwins=0.3333]
-//        {tie=0.272, lose=0.3665, win=0.3614}
-//        [home, pdiff=0.6875, ptwins=0.5]
-//        {tie=0.1899, lose=0.3686, win=0.4416}
-        for (String[] predict : predictData) {
-            System.out.println(Arrays.toString(predict));
-            System.out.println(maxent.predict(predict));
+        List<Map<String, Double>> predictExpected = new ArrayList<>(predictData.size());
+
+        predictExpected.add(getMap("tie=0.1899, lose=0.3686, win=0.4416"));
+        predictExpected.add(getMap("tie=0.2462, lose=0.3451, win=0.4087"));
+        predictExpected.add(getMap("tie=0.2251, lose=0.5947, win=0.1802"));
+        predictExpected.add(getMap("tie=0.1882, lose=0.6056, win=0.2062"));
+        predictExpected.add(getMap("tie=0.2263, lose=0.3535, win=0.4202"));
+        predictExpected.add(getMap("tie=0.2305, lose=0.3859, win=0.3836"));
+        predictExpected.add(getMap("tie=0.2294, lose=0.5656, win=0.205"));
+        predictExpected.add(getMap("tie=0.1688, lose=0.3409, win=0.4903"));
+        predictExpected.add(getMap("tie=0.272, lose=0.3665, win=0.3614"));
+        predictExpected.add(getMap("tie=0.1899, lose=0.3686, win=0.4416"));
+
+
+        for (int i = 0; i < predictData.size(); i++) {
+            String[] predict = predictData.get(i);
+            Map<String, Double> actual = maxent.predict(predict);
+            Map<String, Double> expected = predictExpected.get(i);
+            for (String k : expected.keySet()) {
+                assertTrue(actual.containsKey(k));
+                assertEquals(expected.get(k), actual.get(k), 0.001);
+            }
         }
     }
 }
