38ae7b384d5295334d9efa9dc4cd901b19d9c27e
Maochen G <contact@maochen.org>
2019-03-31 18:26:06 -0700

    update pom

--- CoreNLP-NLP/src/main/java/org/maochen/nlp/parser/stanford/coref/StanfordCoref.java
+++ CoreNLP-NLP/src/main/java/org/maochen/nlp/parser/stanford/coref/StanfordCoref.java
@@ -1,105 +1,105 @@
-package org.maochen.nlp.parser.stanford.coref;
-
-import org.apache.commons.lang3.StringUtils;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.stream.Collectors;
-
-import edu.stanford.nlp.coref.CorefAlgorithm;
-import edu.stanford.nlp.coref.CorefCoreAnnotations;
-import edu.stanford.nlp.coref.data.Dictionaries;
-import edu.stanford.nlp.coref.data.Document;
-import edu.stanford.nlp.coref.data.DocumentMaker;
-import edu.stanford.nlp.coref.data.InputDoc;
-import edu.stanford.nlp.coref.data.Mention;
-import edu.stanford.nlp.coref.neural.NeuralCorefAlgorithm;
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-
-/**
- * Created by Maochen on 4/14/15.
- */
-public class StanfordCoref {
-
-    private final CorefAlgorithm corefAlgorithm;
-    private final Dictionaries dictionaries;
-
-    private Properties props;
-//    private final StanfordNNDepParser parser;
-
-    public StanfordCoref() {
-        try {
-            props = new Properties();
-            dictionaries = new Dictionaries();
-            corefAlgorithm = new NeuralCorefAlgorithm(props, dictionaries);
-        } catch (ClassNotFoundException | IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    public List<String> getCoref(List<String> texts) {
-        Properties props = new Properties();
-        props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, mention");
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-
-        String text = texts.stream().collect(Collectors.joining(StringUtils.SPACE));
-        Annotation annotation = new Annotation(text);
-        pipeline.annotate(annotation);
-
-        InputDoc inputDoc = new InputDoc(annotation);
-        Document document;
-        try {
-            DocumentMaker documentMaker = new DocumentMaker(props, new Dictionaries());
-            document = documentMaker.makeDocument(inputDoc);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        corefAlgorithm.runCoref(document);
-
-        Map<Mention, Mention> pronToNoun = new HashMap<>();
-        document.corefClusters.values().forEach(x -> {
-
-            x.corefMentions.forEach(mentionEntity -> {
-                if (mentionEntity.mentionType == Dictionaries.MentionType.PRONOMINAL) {
-                    pronToNoun.put(mentionEntity, x.representative);
-                }
-            });
-        });
-
-
-        return document.annotation.get(CoreAnnotations.SentencesAnnotation.class).stream().map(singleSentence -> {
-            List<Mention> mentions = singleSentence.get(CorefCoreAnnotations.CorefMentionsAnnotation.class);
-            Map<CoreLabel, String> tokenToWord = new HashMap<>();
-            for (Mention mention : mentions) {
-                if (pronToNoun.containsKey(mention)) {
-                    tokenToWord.put(mention.headIndexedWord.backingLabel(), pronToNoun.get(mention).headString);
-                }
-            }
-
-            return singleSentence.get(CoreAnnotations.TokensAnnotation.class).stream()
-                    .map(token -> {
-                        if (tokenToWord.containsKey(token)) {
-                            String word = tokenToWord.get(token);
-
-                            if (token.get(CoreAnnotations.IndexAnnotation.class) == 1) {
-                                word = StringUtils.capitalize(word);
-                            }
-
-                            return word;
-                        } else {
-                            return token.get(CoreAnnotations.TextAnnotation.class);
-                        }
-                    })
-                    .collect(Collectors.joining(StringUtils.SPACE));
-        }).collect(Collectors.toList());
-    }
-}
+//package org.maochen.nlp.parser.stanford.coref;
+//
+//import org.apache.commons.lang3.StringUtils;
+//
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Properties;
+//import java.util.stream.Collectors;
+//
+//import edu.stanford.nlp.coref.CorefAlgorithm;
+//import edu.stanford.nlp.coref.CorefCoreAnnotations;
+//import edu.stanford.nlp.coref.data.Dictionaries;
+//import edu.stanford.nlp.coref.data.Document;
+//import edu.stanford.nlp.coref.data.DocumentMaker;
+//import edu.stanford.nlp.coref.data.InputDoc;
+//import edu.stanford.nlp.coref.data.Mention;
+//import edu.stanford.nlp.coref.neural.NeuralCorefAlgorithm;
+//import edu.stanford.nlp.ling.CoreAnnotations;
+//import edu.stanford.nlp.ling.CoreLabel;
+//import edu.stanford.nlp.pipeline.Annotation;
+//import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+//
+///**
+// * Created by Maochen on 4/14/15.
+// */
+//public class StanfordCoref {
+//
+//    private final CorefAlgorithm corefAlgorithm;
+//    private final Dictionaries dictionaries;
+//
+//    private Properties props;
+////    private final StanfordNNDepParser parser;
+//
+//    public StanfordCoref() {
+//        try {
+//            props = new Properties();
+//            dictionaries = new Dictionaries();
+//            corefAlgorithm = new NeuralCorefAlgorithm(props, dictionaries);
+//        } catch (ClassNotFoundException | IOException e) {
+//            e.printStackTrace();
+//            throw new RuntimeException(e);
+//        }
+//
+//    }
+//
+//    public List<String> getCoref(List<String> texts) {
+//        Properties props = new Properties();
+//        props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, mention");
+//        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+//
+//        String text = texts.stream().collect(Collectors.joining(StringUtils.SPACE));
+//        Annotation annotation = new Annotation(text);
+//        pipeline.annotate(annotation);
+//
+//        InputDoc inputDoc = new InputDoc(annotation);
+//        Document document;
+//        try {
+//            DocumentMaker documentMaker = new DocumentMaker(props, new Dictionaries());
+//            document = documentMaker.makeDocument(inputDoc);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        corefAlgorithm.runCoref(document);
+//
+//        Map<Mention, Mention> pronToNoun = new HashMap<>();
+//        document.corefClusters.values().forEach(x -> {
+//
+//            x.corefMentions.forEach(mentionEntity -> {
+//                if (mentionEntity.mentionType == Dictionaries.MentionType.PRONOMINAL) {
+//                    pronToNoun.put(mentionEntity, x.representative);
+//                }
+//            });
+//        });
+//
+//
+//        return document.annotation.get(CoreAnnotations.SentencesAnnotation.class).stream().map(singleSentence -> {
+//            List<Mention> mentions = singleSentence.get(CorefCoreAnnotations.CorefMentionsAnnotation.class);
+//            Map<CoreLabel, String> tokenToWord = new HashMap<>();
+//            for (Mention mention : mentions) {
+//                if (pronToNoun.containsKey(mention)) {
+//                    tokenToWord.put(mention.headIndexedWord.backingLabel(), pronToNoun.get(mention).headString);
+//                }
+//            }
+//
+//            return singleSentence.get(CoreAnnotations.TokensAnnotation.class).stream()
+//                    .map(token -> {
+//                        if (tokenToWord.containsKey(token)) {
+//                            String word = tokenToWord.get(token);
+//
+//                            if (token.get(CoreAnnotations.IndexAnnotation.class) == 1) {
+//                                word = StringUtils.capitalize(word);
+//                            }
+//
+//                            return word;
+//                        } else {
+//                            return token.get(CoreAnnotations.TextAnnotation.class);
+//                        }
+//                    })
+//                    .collect(Collectors.joining(StringUtils.SPACE));
+//        }).collect(Collectors.toList());
+//    }
+//}
--- CoreNLP-NLP/src/test/java/org/maochen/nlp/parser/stanford/coref/CorefTest.java
+++ CoreNLP-NLP/src/test/java/org/maochen/nlp/parser/stanford/coref/CorefTest.java
@@ -1,44 +1,44 @@
-package org.maochen.nlp.parser.stanford.coref;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
-
-import org.junit.Test;
-
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-
-
-/**
- * Created by Maochen on 7/20/15.
- */
-public class CorefTest {
-
-    private static StanfordCoref coref = new StanfordCoref();
-
-    @Test
-    public void test() {
-        List<String> texts = Lists.newArrayList("Google invests in companies since they have more choices.", "They understand it is hard.");
-        List<String> actualList = coref.getCoref(texts);
-        List<String> expectedList = ImmutableList.of("Google invests in companies since companies have more choices .", "Companies understand it is hard .");
-
-        for (int i = 0; i < actualList.size(); i++) {
-            assertEquals(expectedList.get(i), actualList.get(i));
-        }
-    }
-
-    @Test
-    public void testStanfordCoref2() {
-        String input1 = "Other important legislation involved economic matters, including the first income tax and higher tariffs.";
-        String input2 = "The Morrill Land-Grant Colleges Act, also signed in 1862, provided government grants for agricultural universities in each state.";
-        List<String> corefed = coref.getCoref(Lists.newArrayList(input1, input2));
-
-        List<String> expected = Lists.newArrayList("Other important legislation involved economic matters , including the first income tax and higher tariffs .",
-                "The Morrill Land-Grant Colleges Act , also signed in 1862 , provided government grants for agricultural universities in each state .");
-
-        for (int i = 0; i < expected.size(); i++) {
-            assertEquals(expected.get(i), corefed.get(i));
-        }
-    }
-}
+//package org.maochen.nlp.parser.stanford.coref;
+//
+//import com.google.common.collect.ImmutableList;
+//import com.google.common.collect.Lists;
+//
+//import org.junit.Test;
+//
+//import java.util.List;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//
+///**
+// * Created by Maochen on 7/20/15.
+// */
+//public class CorefTest {
+//
+//    private static StanfordCoref coref = new StanfordCoref();
+//
+//    @Test
+//    public void test() {
+//        List<String> texts = Lists.newArrayList("Google invests in companies since they have more choices.", "They understand it is hard.");
+//        List<String> actualList = coref.getCoref(texts);
+//        List<String> expectedList = ImmutableList.of("Google invests in companies since companies have more choices .", "Companies understand it is hard .");
+//
+//        for (int i = 0; i < actualList.size(); i++) {
+//            assertEquals(expectedList.get(i), actualList.get(i));
+//        }
+//    }
+//
+//    @Test
+//    public void testStanfordCoref2() {
+//        String input1 = "Other important legislation involved economic matters, including the first income tax and higher tariffs.";
+//        String input2 = "The Morrill Land-Grant Colleges Act, also signed in 1862, provided government grants for agricultural universities in each state.";
+//        List<String> corefed = coref.getCoref(Lists.newArrayList(input1, input2));
+//
+//        List<String> expected = Lists.newArrayList("Other important legislation involved economic matters , including the first income tax and higher tariffs .",
+//                "The Morrill Land-Grant Colleges Act , also signed in 1862 , provided government grants for agricultural universities in each state .");
+//
+//        for (int i = 0; i < expected.size(); i++) {
+//            assertEquals(expected.get(i), corefed.get(i));
+//        }
+//    }
+//}
